dd3bfd12e2b46404224636cd39e68d9b074c938c
LeoAnker0 <lanker013@gmail.com>
2023-04-01 14:30:51 +0100

    through the use of selenium and BS4 now have the page being downloaded from the JSON file

--- scrapeForLinks.py
+++ scrapeForLinks.py
@@ -2,7 +2,88 @@
 #and news pages for links to articles, so that
 #those articles can then be scraped again 
 
+#return the websites JSON file, since it will then need to be fed
+#into other modules
+
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import io
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+import os
+from bs4 import BeautifulSoup
+
+def importScrapeList():
+	with open('scrapeListConfig.json', 'r') as f:
+		data = json.load(f)
+
+	websites = data['websites']
+
+	return websites
+
+def initialise_driver(url):
+	s = Service('./driver/chromedriver')
+
+	options = webdriver.ChromeOptions()
+	options.add_argument("--window-size=1920,1080")
+	options.add_argument("--user-data-dir=/Users/Leo/Library/Application Support/Google/Chrome/Profile 1")
+	options.add_argument("--profile-directory=Profile 1")
+
+	#options.add_argument('--headless')
+	#options.add_argument('--disable-gpu')
+	#options.add_argument('--no-sandbox')
+
+	driver = webdriver.Chrome(service=s, options=options)
+
+	driver.get(url)
+	return driver
+
+def downloadWebPage(website):
+	name = website['name']
+	url = website['url']
+
+	#initialise the driver
+	driver = initialise_driver(url)
+	#perform any page cleaning here
+
+	#get the html from the driver into bs4
+	html_content = driver.page_source
+	soup = BeautifulSoup(html_content, 'html.parser')
+
+	#quit the driver
+	driver.quit()
+
+	return soup
+
 
 
 def scrapeLinks():
 	print(f"scraping links")
+
+
+
+
+if __name__ == "__main__":
+	print("run link scraper")
+
+	websites = importScrapeList()
+
+	for website in websites:
+		soup = downloadWebPage(website)
+		print(soup)
+
+
+
+
+
+
+
+
+
+
+
+
