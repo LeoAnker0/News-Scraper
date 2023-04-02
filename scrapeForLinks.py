687f90a365e1ff7b5ff29185bac0ab7adae8bfd5
LeoAnker0 <lanker013@gmail.com>
2023-04-02 16:28:46 +0100

    started work on remaking the commands system, now have it set up so that development will be easier, since when filtering, i am filtering a downloaded copy of a webpage, rather than downloading it every time, this will save lots of time. However i am tired, so i am not thinking clearly, and i will be pausing for the moment

--- scrapeForLinks.py
+++ scrapeForLinks.py
@@ -73,218 +73,25 @@ def checkIfNumeric(character):
 
 
 def filterWebPage(rules, soup):
-	"""
-	Currently accepted filter rules are as follows
-	starts_with:
-		string: match string (starts_with)
-		command-is_numeric: checks if first character is a number
-		
-	doesnt_start_with:
-		string: ! match string (doesnt_start_with)
-		command-is_not_numeric: checks if first character is not a number
-
-	ends_with:
-		string: match string
-		command-is_numeric: checks if last character is a number
-
-	doesnt_end_with:
-		string: !match string
-		command-is_not_numeric: checks if last character is not a number
-
-	has:
-		string: requires that links have this string in them
-
-	doesnt_have:
-		string: remove all linkks that have this string
-
-	"""
-
 	linksTooFilter = []
 	for link in soup.find_all('a'):
 		link = str(link.get('href'))
 		linksTooFilter.append(link)
 
-	#Starts with
-	#-------------------------------------------------
 
-
-	#check for starts_with_type, if type string, then do string type, 
-	#if none, then do none, if comman, then do command
-
-	starts_with_type = str(rules['starts_with_type'])
-
-	if starts_with_type == "string":
-		#starts with
-		startsWith = str(rules['starts_with'])
-		#new list of only the items wanted
-		startsWithLinksList = []
-
-		lengthOfStart = len(startsWith)
-		for link in linksTooFilter:
-			#this checks if the characters of the string, are equal to the wanted string
-			if link[0:lengthOfStart] == startsWith:
-				#adds to  the list
-				startsWithLinksList.append(link)
-
-		#set main list to be equal to new list
-		linksTooFilter = startsWithLinksList
-
-	elif starts_with_type == "command":
-		command = str(rules['starts_with_command'])
-		#checks if the first character of link is a number, and removes if not
-		if command == "is_numeric":
-			startsWithNumberLinksList = []
-
-			for link in linksTooFilter:
-				firstCharacter = link[0]
-				if checkIfNumeric(firstCharacter) == True:
-					startsWithNumberLinksList.append(link)
-
-			linksTooFilter = startsWithNumberLinksList
+	for command in rules['commands']:
+		print(command)
 
 
 
-
-	#doesn't start with
-	#-------------------------------------------------
-	doesnt_start_with_type = str(rules['doesnt_start_with_type'])
-
-	if doesnt_start_with_type == "string":
-		#starts with
-		doesntStartWith = str(rules['doesnt_start_with'])
-		#new list of only the items wanted
-		doesntStartWithLinksList = []
-
-		lengthOfStart = len(doesntStartWith)
-		for link in linksTooFilter:
-			#checks if the selected strings match
-			if link[0:lengthOfStart] != doesntStartWith:
-				doesntStartWithLinksList.append(link)
-
-		#set the main list to be equal to new list
-		linksTooFilter = doesntStartWithLinksList
-
-
-	elif doesnt_start_with_type == "command":
-		command = str(rules['doesnt_start_with_command'])
-		#checks if the first character of link is a number, and removes if not
-		if command == "is_not_numeric":
-			doesntStartWithLinksList = []
-
-			for link in linksTooFilter:
-				firstCharacter = link[0]
-				if checkIfNumeric(firstCharacter) == False:
-					doesntStartWithLinksList.append(link)
-
-			linksTooFilter = doesntStartWithLinksList
-
-
-
-	#ends with
-	#-------------------------------------------------
-	ends_with_type = str(rules['ends_with_type'])
-
-	if ends_with_type == "string":
-		ends_with = str(rules['ends_with'])
-
-		endsWithLinksList = []
-
-		lengthOfEnd = len(ends_with)
-		for link in linksTooFilter:
-			stringShortened = link[-lengthOfEnd:]
-			if stringShortened == ends_with:
-				endsWithLinksList.append(link)
-
-		linksTooFilter = endsWithLinksList
-
-	elif ends_with_type == "command":
-		command = str(rules['ends_with_command'])
-
-		if command == "is_numeric":
-			endsWithLinksList = []
-
-			for link in linksTooFilter:
-				lastChar = link[-1]
-				if checkIfNumeric(lastChar) == True:
-					endsWithLinksList.append(link)
-
-			linksTooFilter = endsWithLinksList
-
-
-	#doesn't end with
-	#-------------------------------------------------
-	doesnt_end_with_type = str(rules['doesnt_end_with_type'])
-
-	if doesnt_end_with_type == "string":
-		doesnt_end_with = str(rules['doesnt_end_with'])
-
-		doesntEndWithLinksList = []
-
-		lengthOfEnd = len(doesnt_end_with)
-		for link in linksTooFilter:
-			stringShortened = link[-lengthOfEnd:]
-			if stringShortened != doesnt_end_with:
-				doesntEndWithLinksList.append(link)
-
-		linksTooFilter = doesntEndWithLinksList
-
-
-	elif doesnt_end_with_type == "command":
-		command = str(rules['doesnt_end_with_command'])
-
-		if command == "is_not_numeric":
-			doesntEndWithLinksList = []
-
-			for link in linksTooFilter:
-				lastChar = link[-1]
-				if checkIfNumeric(lastChar) == False:
-					doesntEndWithLinksList.append(link)
-
-			linksTooFilter = doesntEndWithLinksList
-
-
-
-
-
-	#doesn't have
-	#-------------------------------------------------
-	#remove all links which have strings matching the doesnt_have array
-	noElementsInDoesntHaveArray = len(rules['doesnt_have'])
-	for i in range(noElementsInDoesntHaveArray):
-		doesntHave = str(rules['doesnt_have'][i])
-		if len(doesntHave) > 0:
-			doesntHaveLinksList = []
-
-			#if doesntHave not in link, then append to doesntHaveLinksList
-			for link in linksTooFilter:
-				if link.find(doesntHave) <=0:
-					doesntHaveLinksList.append(link)
-
-			#set main list to be equal to new list
-			linksTooFilter = doesntHaveLinksList
-
-	#has
-	#-------------------------------------------------
-	#remove all links which dont have strings matching the has array
-	noElementsInHasArray = len(rules['has'])
-	for i in range(noElementsInHasArray):
-		has = str(rules['has'][i])
-		if len(has) > 0:
-			hasLinksList = []
-
-			#if has in link, then append to hasLinksList
-			for link in linksTooFilter:
-				if link.find(has) > 0:
-					hasLinksList.append(link)
-
-			#set main list to be equal to new list
-			linksTooFilter = hasLinksList
 
 
 	#last step is to remove all duplicates, whilst maintaing order
 	linksTooFilter = list(OrderedDict.fromkeys(linksTooFilter))
 
 	return linksTooFilter
+
+
 
 
 def addHTTPStoLinks(rules, linksTooFilter):
@@ -318,6 +125,15 @@ def writeLinksTooFile(name, url, links, targetOutputFile):
 		json.dump(data, file, ensure_ascii=False)
 
 
+def soupifyHTML(source):
+	file = open(source, "r")
+	file_contents = file.read()
+	file.close()
+	soup = BeautifulSoup(file_contents, 'html.parser')
+	return soup
+
+
+
 #for when i'm filtering a new site, and need to make the rules
 def filterNewSite():
 	print(f"adding new site to be scraped")
@@ -325,10 +141,11 @@ def filterNewSite():
 
 	websites = importScrapeList(scrapeListFile)
 	for website in websites:
-		soup = downloadWebPage(website)
+		#soup = downloadWebPage(website)
+		soup = soupifyHTML('test.html')
 		links = filterWebPage(website, soup)
-		for link in links:
-			print(link)
+		#for link in links:
+	#		print(link)
 
 #the normal procedure for scraping links from a page
 def filterFromExistingScrapeList(scrapeListFile, targetLinksOutputFile):
