1af9093aadbf2cee0430e978a1574b888ebe0c8a
LeoAnker0 <lanker013@gmail.com>
2023-04-03 11:05:05 +0100

    added the first of the new filters in the new filtering system
    created a function which returns a sub string of the input string (link), which can then be used in the further operations
    
    added the parsers and filters that accept the new commands
    
    still need to add all of the different things like is_numeric and so on, however my goal at the moment is to add as many sites as possible, and then check wether the current filters are sufficient, because really it would be a shame for it to be static, since it really would be best for it to be very fluid and easy to add new filters, since that will be the reality, sort of
    
    stil stuff to do, but i'm happy with the current progress

--- scrapeForLinks.py
+++ scrapeForLinks.py
@@ -73,18 +73,130 @@ def checkIfNumeric(character):
 
 
 def filterWebPage(rules, soup):
+	print()
 	linksTooFilter = []
 	for link in soup.find_all('a'):
 		link = str(link.get('href'))
 		linksTooFilter.append(link)
 
+	#get a string for comparison, based on the input criteria
+	def get_string(howMany, lengthOfContent, link, where):
+		#if howMany is all or any
+		if howMany == "all":
+			if where == "start":
+				try:
+					return_string = link[0 : int(lengthOfContent)]
+					#print(return_string)
+					return return_string
+				except IndexError:
+					print("the indexs are out of range")
+					return "::skip::"
+			#gets the string from the end, rather than the beginning
+			elif where == "end":
+				try:
+					return_string = link[-int(lengthOfContent):]
+					#print(return_string)
+					return return_string
+				except IndexError:
+					print("the indexes are out of range")
+					return "::skip::"
 
+		#if howMany is just a normal number
+		elif howMany.isdigit():
+			if where == "start":
+				try:
+					return_string = link[0: int(howMany)]
+					#print(return_string)
+					return return_string
+				except IndexError:
+					print("the indexes are out of range")
+					return "::skip::"
+			if where == "end":
+				try:
+					return_string = link[-int(howMany):]
+					#print(return_string)
+					return return_string
+				except IndexError:
+					print("the indexes are out of range")
+					return "::skip::"
+
+		elif "," in howMany:
+			numbers = howMany.split(",")
+			number1 = numbers[0]
+			number2 = numbers[1]
+
+			try:
+				number1 = int(number1)
+			except ValueError:
+				number1 = None if number1 == "None" else number1
+
+			try:
+				number2 = int(number2)
+			except ValueError:
+				number2 = None if number2 == "None" else number2
+
+			try:
+				return_string = link[number1:number2]
+				#print(return_string)
+				return return_string
+			except IndexError:
+				print("the indexes are out or range")
+				return "::skip::"
+
+		else:
+			print(f"there was any invalid input in get_start_string")
+			return "::skip::"
+
+
+	#for every command in the rules, do action
 	for command in rules['commands']:
-		print(command)
+		array_of_commands = command.split(":")
+
+		#split the array of commands into seperate variables
+		where = array_of_commands[0] 	#start/end/in?
+		how_many = array_of_commands[1]	#how many of those characters to check/slice indicing
+		condition = array_of_commands[2]#wether it should be true or false
+		what = array_of_commands[3]		#what it is that is being checked
+		content = array_of_commands[4]	#a condition or value that is relevant to the what
+
+		#intialise new array
+		linksList = []
+
+		#do something with them
+		for link in linksTooFilter:
+			#if where is start or end
+			if where == "start" or where == "end":
+				return_string = get_string(how_many, len(content), link, where)
+				if return_string == "::skip::":
+					continue
+
+				#if what is string, and condition is true
+				if what == "string" and condition == "true":
+					if return_string == content:
+						linksList.append(link)
+						#print(f"return_string : {return_string}")
+				#if what is string and condition is false
+				elif what == "string" and condition == "false":
+					if return_string != content:
+						linksList.append(link)
+						#print(f"return_string : {return_string}")
+
+			#if where is in
+			elif where == "in":
+				#if check == string and condition == true
+				if what == "string" and condition == "true":
+					if content in link:
+						#print(link)
+						linksList.append(link)
+				#if check == string and condition == false
+				if what == "string" and condition == "false":
+					if content not in link:
+						#print(link)
+						linksList.append(link)
 
 
-
-
+		#then set linksTooFilter to equal linksList
+		linksTooFilter = linksList
 
 	#last step is to remove all duplicates, whilst maintaing order
 	linksTooFilter = list(OrderedDict.fromkeys(linksTooFilter))
@@ -156,8 +268,11 @@ def filterFromExistingScrapeList(scrapeListFile, targetLinksOutputFile):
 		websiteURL = websiteRules['url']
 		soup = downloadWebPage(websiteRules)
 		links = filterWebPage(websiteRules, soup)
-		links = addHTTPStoLinks(websiteRules, links)
-		writeLinksTooFile(websiteName, websiteURL, links, targetLinksOutputFile)
+
+		for link in links:
+			print(link)
+		#links = addHTTPStoLinks(websiteRules, links)
+		#writeLinksTooFile(websiteName, websiteURL, links, targetLinksOutputFile)
 
 
 if __name__ == "__main__":
@@ -166,8 +281,8 @@ if __name__ == "__main__":
 	scrapeListFile = "scrapeListConfig.json"
 	targetLinksOutputFile = "links.json"
 
-	#filterFromExistingScrapeList(scrapeListFile, targetLinksOutputFile)
-	filterNewSite()
+	filterFromExistingScrapeList(scrapeListFile, targetLinksOutputFile)
+	#filterNewSite()
 
 
 
