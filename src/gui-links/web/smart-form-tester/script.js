5a56a7997fe1a0a0bec1bfc6c7304d31a2addd91
Leo <lanker013@gmail.com>
2023-04-23 23:55:04 +0100

    i now have a pretty solid starting point for the form, and having now tested it in it's own folder i will now be moving it into the main project, and just hoping for the best
    But it's actually pretty cool, there's auto suggestion with autofill upon hitting enter
    
    The add multiple rows button works, but there are a few things that need to fixed first and foremost
    
    (or not fixed but added)
    
    it needs to stay as it is upon load in, and when ever the enter button is hitten, it should not refresh itself, but i think these are easy remedies
    
    what will be harder is adding error checking and input validation/correction, which will be added when it's at the other place, because that's just much smarter in most ways
    
    plus that allows me the chance to build the system in alignment with the other 3 fields that need to be there
    
    but yeah, a solid starting point for integrating into the linkScraperGui

--- src/gui-links/web/smart-form-tester/script.js
+++ src/gui-links/web/smart-form-tester/script.js
@@ -1,36 +1,124 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("custom-form");
+const positions = ["end", "start", "in"];
+const lengths = ['all'];
+const conditions = ["true", "false"];
+const commands = ["string", "is_numeric", "count"];
+const contents = ['all'];
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const position = form.querySelector(".position").value;
-        const length = form.querySelector(".length").value;
-        const condition = form.querySelector(".condition").value;
-        const command = form.querySelector(".command").value;
-        const content = form.querySelector(".content").value;
+window.onload = () => {
+    const addButton = document.getElementById("add-button");
+    addButton.addEventListener("click", addInputGroup);
 
-        // Perform custom validation and filtering based on input values
-        const finalContent = filterContent(position, length, condition, command, content);
+    // Create initial input group
+    addInputGroup();
+};
 
-        // Combine input values into a single string
-        const result = `${position},${length},${condition},${command},${finalContent}`;
+function addInputGroup() {
+    const inputGroup = document.createElement("div");
+    inputGroup.classList.add("input-group");
 
-        console.log(result);
-    });
-});
+    // Create position input with datalist
+    const positionInput = document.createElement("input");
+    positionInput.classList.add("position");
+    positionInput.type = "text";
+    positionInput.placeholder = "Position";
+    positionInput.setAttribute("list", "position-options");
+    positionInput.autocomplete = "off";
+    inputGroup.appendChild(positionInput);
 
-function filterContent(position, length, condition, command, content) {
-    // Add your custom filters based on input values
-    // For example, if position is "end", modify content in some way
-    if (position === "end") {
-        content += "_end";
-    }
+    const lengthInput = document.createElement("input");
+    lengthInput.classList.add("length");
+    lengthInput.type = "text";
+    lengthInput.placeholder = "Length";
+    lengthInput.autocomplete = "off";
+    inputGroup.appendChild(lengthInput);
 
-    // Add more conditions and custom filters as needed
-    // For example, if command is "string" and content contains numbers, remove them
-    if (command === "string") {
-        content = content.replace(/[0-9]/g, '');
-    }
+    // Create condition input with datalist
+    const conditionInput = document.createElement("input");
+    conditionInput.classList.add("condition");
+    conditionInput.type = "text";
+    conditionInput.placeholder = "Condition";
+    conditionInput.setAttribute("list", "condition-options");
+    conditionInput.autocomplete = "off";
+    inputGroup.appendChild(conditionInput);
 
-    return content;
+    // Create command input with datalist
+    const commandInput = document.createElement("input");
+    commandInput.classList.add("command");
+    commandInput.type = "text";
+    commandInput.placeholder = "Command";
+    commandInput.setAttribute("list", "command-options");
+    commandInput.autocomplete = "off";
+    inputGroup.appendChild(commandInput);
+
+    const contentInput = document.createElement("input");
+    contentInput.classList.add("content");
+    contentInput.type = "text";
+    contentInput.placeholder = "Content";
+    contentInput.autocomplete = "off";
+    inputGroup.appendChild(contentInput);
+
+    document.getElementById("input-groups-container").appendChild(inputGroup);
+
+    // Set up suggestions for newly created input elements
+    setupSuggestions(positionInput, positions);
+    setupSuggestions(conditionInput, conditions);
+    setupSuggestions(commandInput, commands);
+    setupSuggestions(lengthInput, lengths);
+    setupSuggestions(contentInput, contents);
 }
+
+// Helper function to set up suggestions for a field
+function setupSuggestions(input, options) {
+    const suggestion = document.createElement("span");
+    suggestion.classList.add("suggestion");
+    input.parentNode.appendChild(suggestion);
+
+    input.addEventListener("input", (e) => {
+        const inputText = e.target.value;
+        suggestion.textContent = findSuggestion(inputText, options);
+        positionSuggestion(input, suggestion);
+    });
+
+    input.addEventListener("blur", () => {
+        suggestion.textContent = "";
+    });
+
+    // Listen for the Enter key press to autofill the suggestion
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Enter" && suggestion.textContent) {
+            e.preventDefault(); // Prevent form submission if the input is in a form
+            input.value = suggestion.textContent;
+            suggestion.textContent = "";
+        }
+    });
+}
+
+
+function findSuggestion(input, options) {
+    if (!input) return "";
+
+    for (const option of options) {
+        if (option.startsWith(input)) {
+            return option;
+        }
+    }
+
+    return "";
+}
+
+function positionSuggestion(input, suggestion) {
+    // Adjust these variables to change the position of the suggestions
+    const suggestionOffsetX = 0; // Move the suggestion horizontally
+    const suggestionOffsetY = -23; // Move the suggestion vertically
+
+    const inputCoords = input.getBoundingClientRect();
+    suggestion.style.top = `${
+    inputCoords.top + inputCoords.height + suggestionOffsetY
+  }px`;
+    suggestion.style.left = `${
+    inputCoords.left + inputCoords.width / 2 - suggestion.offsetWidth / 2 + suggestionOffsetX
+  }px`;
+}
+
+
+//
